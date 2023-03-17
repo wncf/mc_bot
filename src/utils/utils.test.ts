87e1f89c7ba59bfb0a9c16dfc521d0333e6b46a0
wncf <2630473215@qq.com>
2023-03-17 11:02:31 +0800

    jest 测试

--- src/utils/utils.test.ts
+++ src/utils/utils.test.ts
@@ -1,10 +1,6 @@
 // 测试用例的写法
-// import { sum } from '../index'
-
-import { getMcServerInfo } from '@/utils/request'
-import path from 'path'
-import { getMcConfig, powerFun, settingCommand } from './index'
-
+import { handelmcResultInfo } from './index'
+import { getMcServerInfo } from './request'
 describe('集成测试Get_mc', () => {
   it('测试获取服务器json信息', async () => {
     const groupId = 512772656
@@ -12,13 +8,10 @@ describe('集成测试Get_mc', () => {
     // const data = new settingCommand(512772656, '-add  name=e2e address=323232 alias=e2e')
     // const data = new settingCommand(512772656, '-update  default address=323232 alias=e2e')
     // const data = new settingCommand(512772656, '-remove  e2e ')
-
-    // console.log(path.resolve(process.cwd(), '.env'))
-    // console.log(path.resolve('<rootDir>/src/'))
-    // console.log(path.resolve('mc_config.json'))
-
+    const res = await getMcServerInfo('h.lkeme.top:12306')
+    const data = new handelmcResultInfo(res,'梦醒')
+    console.log(data)
     // console.log(pathsa)
-
     expect(0).toEqual(0)
   })
 })
